c2781be920388af8b39979ca6a0d65ac9e1692ba
Ally0522 <allyleena05@gmail.com>
2023-10-18 09:42:40 +0000

    count.index

--- variables/count.tf
+++ variables/count.tf
@@ -1,10 +1,13 @@
-/*
 provider "aws" {
   region = "us-east-1"
   }
 
 resource "aws_instance" "test" {
   ami = "ami-067d1e60475437da2"
-  instance_type = var.list[0]
+  instance_type = var.list[1]
+  count = 4
+
+tags = {
+    Name = var.instance_name[count.index]
 }
-*/
+}
--- variables/variables.tf
+++ variables/variables.tf
@@ -13,7 +13,6 @@ variable "az" {
 variable "timeout" {
     type = number
 }
-*/
 
 variable "types" {
     type = map
@@ -22,8 +21,14 @@ variable "types" {
         us-west-1 = "t2.small"
         us-east-2 = "t2.medium"
     }
-}
+    */
+
 variable "list" {
     type = list
     default = ["m5.large","m5.xlarge","t2.medium"]
+}
+
+variable "instance_name"{
+    type = list
+    default = ["dev01","dev02","prod01","prod02"]
 }
