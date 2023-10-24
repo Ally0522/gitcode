a6b38f4b23b13bea6712a349425049d50c6987a4
Allyleena0522 <allyleena05@gmail.com>
2023-10-24 20:51:53 +0530

    datasource_update

--- ec2.tf
+++ ec2.tf
@@ -5,17 +5,17 @@ provider "aws" {
 
 data "aws_ami" "app_ami" {
   most_recent = true
-  owners = "amazon"
+  owners = ["amazon"]
 
   filter {
     name = "name"
-    values = [ "amzn2-ami-hvm" ]
+    values = [ "amzn2-ami-hvm*" ]
 
   }
   
 }
 
 resource "aws_instance" "test" {
-  ami = "data.aws_ami.app_ami.id"
+  ami = data.aws_ami.app_ami.id
   instance_type = "t2.micro"
 }
